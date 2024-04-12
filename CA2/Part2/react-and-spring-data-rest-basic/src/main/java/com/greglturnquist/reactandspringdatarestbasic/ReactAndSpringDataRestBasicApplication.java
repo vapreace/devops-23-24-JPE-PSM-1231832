538b7c0956ef7ad760595924fe8e893a2e71253f
ineslemos <ines__lemos@hotmail.com>
2024-04-12 23:54:51 +0100

    [setup] Fixes #8 - Initialize Gradle project setup

---
+++ CA2/Part2/react-and-spring-data-rest-basic/src/main/java/com/greglturnquist/reactandspringdatarestbasic/ReactAndSpringDataRestBasicApplication.java
@@ -0,0 +1,13 @@
+package com.greglturnquist.reactandspringdatarestbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactAndSpringDataRestBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactAndSpringDataRestBasicApplication.class, args);
+	}
+
+}
