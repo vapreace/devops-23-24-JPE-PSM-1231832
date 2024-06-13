0c9b13f4a1aca88d3ff3f612c215a27434ed6a3a
ineslemos <ines__lemos@hotmail.com>
2024-06-13 11:36:40 +0100

    [feat] #30 - updated Jenkinsfile (.war files)

--- CA2/Part2/react-and-spring-data-rest-basic/src/main/resources/static/built/bundle.js
+++ CA2/Part2/react-and-spring-data-rest-basic/src/main/resources/static/built/bundle.js
@@ -34124,7 +34124,7 @@ var App = /*#__PURE__*/function (_React$Component) {
       // <2>
       client({
         method: 'GET',
-        path: '/api/employees'
+        path: '/basic-0.0.1-SNAPSHOT/api/employees'
       }).done(function (response) {
         _this2.setState({
           employees: response.entity._embedded.employees
